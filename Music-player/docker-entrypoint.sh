1ce34d63a2f98d360a5e54971eae5df51de110d2
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-11-29 15:57:17 +0800

    Update docker-entrypoint.sh

--- Music-player/docker-entrypoint.sh
+++ Music-player/docker-entrypoint.sh
@@ -10,10 +10,30 @@ echo -e "===================3. check.sh ===========\n"
 ./check.sh &
 echo -e "check.sh启动成功...\n"
 
-
-
 # start unblock service in the background
-npx unblockneteasemusic -p 80:443 -s -f ${NETEASE_SERVER_IP:-220.197.30.65} -o ${UNBLOCK_SOURCES:-kugou bodian pyncmd} 
+npx unblockneteasemusic -p 543:443 -s -f ${NETEASE_SERVER_IP:-220.197.30.65} -o ${UNBLOCK_SOURCES:-kugou bodian pyncmd} 2>&1 &
+
+# point the neteasemusic address to the unblock service
+if ! grep -q "music.163.com" /etc/hosts; then
+    echo "127.0.0.1 music.163.com" >> /etc/hosts
+fi
+if ! grep -q "interface.music.163.com" /etc/hosts; then
+    echo "127.0.0.1 interface.music.163.com" >> /etc/hosts
+fi
+if ! grep -q "interface3.music.163.com" /etc/hosts; then
+    echo "127.0.0.1 interface3.music.163.com" >> /etc/hosts
+fi
+if ! grep -q "interface.music.163.com.163jiasu.com" /etc/hosts; then
+    echo "127.0.0.1 interface.music.163.com.163jiasu.com" >> /etc/hosts
+fi
+if ! grep -q "interface3.music.163.com.163jiasu.com" /etc/hosts; then
+    echo "127.0.0.1 interface3.music.163.com.163jiasu.com" >> /etc/hosts
+fi
+
+# start the nginx daemon
+nginx
+
+
 
 
 
