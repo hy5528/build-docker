115e6746cc62d879f7508d9f03d672ef56950035
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-12-06 21:11:10 +0800

    Update moode_docker.sh

--- musicapi/moode_docker.sh
+++ musicapi/moode_docker.sh
@@ -71,7 +71,7 @@ echo "*********************************************"
 echo ""
 sleep 1
 
-podman exec -ti debian-moode /bin/bash -c "curl -1sLf  'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' | sudo -E distro=raspbian codename=bullseye arch=armv7hf bash -"
+podman exec -ti debian-moode /bin/bash -c "curl -1sLf  'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' | sudo -E distro=raspbian codename=bullseye arch=arm64 bash -"
 podman exec -ti debian-moode /bin/bash -c "apt-get update -y"
 podman exec -ti debian-moode /bin/bash -c "apt-get install udisks nginx triggerhappy samba dnsmasq -y"
 echo ""
