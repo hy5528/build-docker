bdf1d209b512c3b9a520c18631eb831472ca084a
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-07-01 09:47:29 +0800

    Create build-config.ts

---
+++ quicktv/src/config/build-config.ts
@@ -0,0 +1,10 @@
+export default {
+  // MacCMS API 地址
+  requestBaseUrl: 'https://cj.lziapi.com/api.php/provide/vod',
+  
+  // 应用包名
+  packageName: 'es.tv.huan.hellotv',
+  
+  // 是否使用模拟数据
+  useMockData: false,
+}
