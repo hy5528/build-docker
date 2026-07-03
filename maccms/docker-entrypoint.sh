851de98c45c56044771927e8a8ecc863f5f748bb
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-07-03 16:54:38 +0800

    Add files via upload

---
+++ maccms/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+if [ ! -f index.php ]; then
+    git clone --depth 1 -q ${REPO_URL} .
+    rm -rf .git*
+    chmod a+rw -R application runtime upload static addons
+    echo "maccms10 downloaded"
+fi
+
+if [ -f admin.php ]; then
+    mv admin.php $ADMIN_PHP
+    echo "admin.php => $ADMIN_PHP"
+fi
+
+exec "$@"
